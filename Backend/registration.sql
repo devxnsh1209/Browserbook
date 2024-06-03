67f75b03589729e0f27bfd3beef31d8f218e4f40
devxnsh1209 <100987818+devxnsh1209@users.noreply.github.com>
2024-06-03 09:58:16 +0530

    Add files via upload

---
+++ Backend/registration.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Registration
+(
+    FullName varchar(255),
+    Email varchar(255),
+    Contact varchar(255),
+    City varchar(255),
+	Username varchar(255),
+    Passwords varchar(255),
+    ConfirmPasswords varchar(255)
+);
+INSERT INTO Registration (FullName, Email, Contact)
+VALUES ('Utsav','utsavkatharotiya0712@gmail.com','7984281880');
